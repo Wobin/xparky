f6e363dd3974b58a41c8bfb9bac110b96b7553b9
next96 <next96@48e1f902-e210-0410-a839-a0138e9d5bd5>
2008-02-27 16:26:04 +0000

    Xparky : - Update locale koKR
    git-svn-id: http://dev.wowace.com/wowace/trunk/Xparky@63064 48e1f902-e210-0410-a839-a0138e9d5bd5

--- Locale/ko_KR.lua
+++ Locale/ko_KR.lua
@@ -13,6 +13,12 @@ if L then
 
 	L["Show Shadow"] = "그림자 보기"
 	L["Attach a shadow to the bars"] = "바에 그림자를 표시합니다."
+	
+	L["Show Legoblock"] = "설명 표시"
+	L["Give a textbox with xp/rep details"] = "경험치/평판에 대한 상세 정보 창을 표시합니다."
+	
+	L["XP/Rep to go"] = "필요 경험치/평판"
+	L["Show the amount present or the amount to go"] = "현재 경험치/평판과 다음 레벨까지 필요한 경험치/평판을 표시합니다."
 
 	L["XP Spark Intensity"] = "경험치 광채 밝기"
 	L["How strong the XP spark is"] = "경험치바의 광채 밝기를 조절합니다."
@@ -21,7 +27,14 @@ if L then
 
 	L["Bar Thickness"] = "바 두께"
 	L["How thick the bars are"] = "표시되는 바의 두께를 변경합니다."
+	
+	L["Hide Bars"] = "바 숨기기"
+	L["Hide the bars til you mouseover them"] = "마우스로 확인 전까지 바를 숨깁니다."
+	L["Show Tooltip"] = "툴팁 표시"
+	L["Show a tooltip with the XP/Rep info when moused over"] = "바에 마우스를 올리면 경험치/평판에 관한 툴팁을 표시합니다."
 
+	L["Attachment Method"] = "접근 방법"
+	L["Frame Link"] = "프레임 링크"
 	L["Hook to frame"] = "프레임 선택기"
 	L["Click here to activate the frame selector"] = "마우스로 클릭하여 프레임 선택기를 활성화 합니다."
 	L["Frame Connected to"] = "프레임 선택"
@@ -31,9 +44,16 @@ if L then
 	L["Which side to attach to"] = "프레임의 상/하 어디에 바를 표시할지 선택합니다."
 	L["Top"] = "위"
 	L["Bottom"] = "아래"
+	L["Left"] = "왼쪽"
+	L["Right"] = "오른쪽"
 	L["Inside Frame?"] = "프레임 내부 표시"
 	L["Attach to the inside of the frame"] = "해당 프레임의 내부에 바를 표시합니다."
-
+	L["Bar Strata"] = "바 위치 우선순위"
+	L["Set the Bar Strata so it appears above or below other frames"] = "바를 어느 프레임에 우선하여 표시할지 설정합니다."
+	L["X offset"] = "가로 기준위치"
+	L["How far on the X axis to offset the bars"] = "바가 가로로 어느 기준위치에 표시될지 변경합니다."
+	L["Y offset"] = "세로 기준위치"
+	L["How far on the Y axis to offset the bars"] = "바가 세로로 어느 기준위치에 표시될지 변경합니다."
 
 	L["Colours"] = "색상"
 	L["Colours of the bars"] = "바의 색상을 변경합니다"
@@ -48,6 +68,15 @@ if L then
 	L["Empty Reputation Bar"] = "남은 평판바"
 	L["Colour of the empty Reputation bar"] = "남은 평판바의 색상을 변경합니다."
 
+	L["Factions"] = "평판"
 	L["Faction Selected"] = "평판 선택"
 	L["List of Factions to watch"] = "평판바에 표시할 평판을 선택합니다."
+
+	L[" xp to go"] = " (다음 레벨까지 경험치)"
+	L[" rep to go - "] = "다음 관계까지의 평판치 - "
+
+	L["Documentation"] = "문서"
+	L["Description"] = "설명"
+	L["About"] = "대하여"
+	L["FAQ"] = "빠른 질문/답변"
 end
